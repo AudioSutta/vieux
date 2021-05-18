4d75744b6b19a31167b347b55b1ae3be8e544167
AudioSutta <84146971+AudioSutta@users.noreply.github.com>
2021-05-18 13:07:10 +0100

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,10 +1,10 @@
 const themeMap = {
-  'default': {
+  'sombre': {
      'background-color': '#222',
     'text-color': 'white',
     'highlight-color': '#2e2e2e'
   },
-  'dark': {
+  'claire': {
     'background-color': 'white',
     'text-color': '#222',
     'highlight-color': '#eee'
