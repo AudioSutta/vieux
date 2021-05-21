ba046413a2a82124ddf323e3627f1b8a7ec67adc
AudioSutta <84146971+AudioSutta@users.noreply.github.com>
2021-05-21 21:27:15 +0100

    Create theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,12 @@
+const themeMap = {
+  'sombre': {
+    'background-color': '#222',
+    'text-color': 'white',
+    'highlight-color': '#2e2e2e'
+  },
+  'foncé': {
+    'background-color': 'white',
+    'text-color': '#222',
+    'highlight-color': '#eee'
+  }
+}
